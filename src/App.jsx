124920d09d2c9a7a800927e85bd82a25f86f7c81
Misterleo <misterleo.minecraft@gmail.com>
2025-10-30 20:50:01 +0100

    menu di accesso veloce

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,22 @@ import PC from './PC.jsx';
 import Loading from './components/Loading.jsx';
 import './App.css'
 import PDFPage from './components/PDFPage.jsx';
+import { Link } from "react-router-dom";
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<div>Home</div>}/>
+        <Route
+          path="/"
+          element={
+            <div>
+              <ul>
+                <li><Link to="/Esistenza_incompiuta_vol1">Esistenza incompiuta</Link></li>
+                <li><Link to="/Power_chess">Power chess</Link></li>
+              </ul>
+            </div>}/>
         <Route path='/caricamento' element={<Loading/>}/>
         <Route path='/Power_chess' element={<PC/>}/>
         <Route path='/Esistenza_incompiuta_vol1' element={<EIUno/>}/>
